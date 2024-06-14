5289295899b04f928e28459c551bc14a73fcadde
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2024-06-14 12:32:25 -0300

    remoçãode comentários

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from src.problems import solve_problem_1, solve_problem_4, solve_problem_7
-# from problems.problem_4 import solve_problem_4 
-# from problems.problem_7 import solve_problem_7
 from src.results.plot_results import plot_results
 
 def main():
+
     # Mapeando números de problemas para as funções de solução correspondentes
     problems_map = {
         1: solve_problem_1,
@@ -40,23 +39,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# result_1 = solve_problem_1()
-# print(f'Problema 1 - Status: {result_1.message}')
-# if result_1.success: 
-#     print(f'Solução ótima: x* = {result_1.x[0]}, f(x) = {result_1.x[1]}')
-#     print(f'Valor da função objetivo na solução ótima: {result_1.fun}')
-# else:
-#     print(f'Solução não encontrada')
-
-# result_4 = solve_problem_4()
-# print(f'Problema 4 - Status: {result_4.message}')
-# if result_4.success: 
-#     print(f'Solução ótima: x* = {result_4.x[0]}, f(x) = {result_4.x[1]}')
-#     print(f'Valor da função objetivo na solução ótima: {result_4.fun}')
-# else:
-#     print(f'Solução não encontrada')
-
-# result_7 = solve_problem_7()
-# print(f'Problema 7 - Solução ótima: x* = {result_7[0][0]}, f(x) = {result_7[0][1]}')
-# print(f'Valor da função objetivo na solução ótima: {result_7[1]}')
